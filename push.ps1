5b17a324125453f9857bd2d0d5e1fadb21df84b4
huhuiyu <1069306849@qq.com>
2023-09-11 08:18:08 +0800

    多仓库推送脚本

--- push.ps1
+++ push.ps1
@@ -1,2 +1,7 @@
+# github链接地址：git remote add github git@github.com:huhuiyu/sql.git
+# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/sql.git
+# gitee链接地址：git remote add gitee git@gitee.com:huhuiyu/sql.git
 git pull
 git push
+git push jihulab
+git push gitee
